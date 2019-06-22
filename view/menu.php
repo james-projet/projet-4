f3d1e68ad86bade21a5ea97b93d21ef9f779f6d2
james <james.gaffe@yahoo.fr>
2019-06-23 01:22:55 +0200

    menu

---
+++ view/menu.php
@@ -0,0 +1,18 @@
+<nav class="menu">
+      <ol>
+        <li class="menu-item"><a href="<?= HOST?>homepage">A Propos...</a></li>
+        <li class="menu-item">
+          <a href="#0">Mon Livre</a>
+          <ol class="sub-menu">
+            <li class="menu-item"><a href="#0"></a></li>
+            <li class="menu-item"><a href="#0">Bigger Widgets</a></li>
+            <li class="menu-item"><a href="#0">Huge Widgets</a></li>
+          </ol>
+        </li>
+        <li class="menu-item"><a href="<?= HOST?>signin">Se connecter</a></li>
+        <li class="menu-item"><a href="<?= HOST?>contact">Me Contacter</a></li>
+        <li class="menu-item"><a href="<?= HOST?>showcomsignaler">Commentaire signaler</a></li>
+        <li class="menu-item"><a href="<?= HOST?>editepisode">Nouveau chapitre</a></li>
+        <li class="menu-item"><a href="<?= HOST?>editepisode">Se deconnecter</a></li>
+      </ol>
+    </nav>
