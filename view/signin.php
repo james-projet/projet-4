0be04a515ba64c3f97b09c92f7666dc321c74b42
james <james.gaffe@yahoo.fr>
2019-06-23 01:23:50 +0200

    connexion

---
+++ view/signin.php
@@ -0,0 +1,12 @@
+<form class="" action="login" method="post" class="form">
+  <div class="">
+    <input placeholder="pseudo" type="text" name="pseudo" id="pseudoedit" />
+  </div>
+  <div class="">
+    <input placeholder="mot de passe" type="text" name="mdp" id="mdpedit" />
+  </div>
+  <div class="">
+    <input type="submit" value="Envoyer" id="buttonedit">
+  </div>
+</form>
+<a href="<?= HOST?>deco">deco</a>
