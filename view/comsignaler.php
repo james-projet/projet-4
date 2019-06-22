44aef72015bfa470774ed68313f5f8b2a8b503f2
james <james.gaffe@yahoo.fr>
2019-06-23 01:17:57 +0200

    signaler les commentaire

---
+++ view/comsignaler.php
@@ -0,0 +1,16 @@
+<?php if (isset($_SESSION['id']) and isset($_SESSION['pseudo'])) {
+   echo "<p>Bonjour, " . $_SESSION['pseudo'];
+   foreach ($commentaires as $commentaire):?>
+    <div class="espacecom">
+      <div class="com">
+        <p><span class="pseudocom"><?= $commentaire->getPseudo()?></span><span class="datecom"> il y a <?= $commentaire->getDate_creation();?></span></p>
+
+        <p class="messagecom"><?= $commentaire->getCommentaire();?></p>
+      </div>
+      <div class="signaler-com">
+        <a class="liencom" href="<?= HOST?>deletecom/id/<?= $commentaire->getId();?>" aria-label="supprimer le commentaire"><i class="fas fa-trash-alt"></i></a>  
+      </div>
+    </div>
+
+  <?php endforeach;
+} ?>
