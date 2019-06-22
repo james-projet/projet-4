e0b09413fecae54228decb41d2ed32e87bf37d23
james <james.gaffe@yahoo.fr>
2019-06-23 01:20:12 +0200

    footer

---
+++ view/footer.php
@@ -0,0 +1,18 @@
+<div class="" id="listfooter">
+  <div class="" id="listplus">
+    <ul>
+      <li> <strong>en savoir plus</strong> </li>
+      <li>politique de confidentialités</li>
+      <li>nous contacter</li>
+    </ul>
+  </div>
+  <div class="" id="listreseaux">
+    <ul>
+      <li> <strong>nos reseaux</strong> </li>
+      <li> <img src="<?= HOST?>public/photos/fb.png" alt="logo" class="reseaux"> </li>
+      <li> <img src="<?= HOST?>public/photos/insta.jpg" alt="logo" class="reseaux"> </li>
+      <li> <img src="<?= HOST?>public/photos/twitter.jpg" alt="logo" class="reseaux"> </li>
+    </ul>
+  </div>
+
+</div>
