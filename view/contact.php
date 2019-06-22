b4045ed94be4cbca7403a6065fb91c8f07d4e7e4
james <james.gaffe@yahoo.fr>
2019-06-23 01:18:54 +0200

    contact

---
+++ view/contact.php
@@ -0,0 +1,18 @@
+<p id="textcontact">N'hesiter pas a me contacter si vous avez des retours a me faire ou des projet a me proposer</p>
+
+<form id="formcontact" action="contact" method="post">
+  <div class="">
+    <input placeholder="nom" type="text" name="nom" id="nomcontact" />
+  </div>
+  <div class="">
+    <input placeholder="email@exemple.fr" type="text" name="prenom" id="emailcontact" />
+  </div>
+  <div class="">
+    <textarea placeholder="message" type="text" name="prenom" id="messagecontact"></textarea>
+  </div>
+  <div class="">
+    <input type="submit" value="Envoyer" id="buttoncontact">
+  </div>
+
+
+</form>
