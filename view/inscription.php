58aaef3702dcac4b291e9c73306b85a6489a51f4
james <james.gaffe@yahoo.fr>
2019-06-23 01:22:36 +0200

    inscription

---
+++ view/inscription.php
@@ -0,0 +1,12 @@
+
+<form class="" action="<?= HOST?>inscrire" method="post" id="formedit">
+  <div class="">
+    <input placeholder="pseudo" type="text" name="pseudo"/>
+  </div>
+  <div class="">
+    <input placeholder="mot de passe" type="text" name="mdp"/>
+  </div>
+  <div class="">
+    <input type="submit" value="Envoyer">
+  </div>
+</form>
