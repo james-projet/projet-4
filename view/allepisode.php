9fbd278fc96cd91c7ecb0a953fa20a659ddcf1e1
james <james.gaffe@yahoo.fr>
2019-06-24 03:21:50 +0200

    affiche tous les episodes par 5

---
+++ view/allepisode.php
@@ -0,0 +1,49 @@
+<div class="all-episode">
+    <div class="image-episode">
+      <img src="<?= HOST?>public/photos/undraw_viral_tweet_gndb.svg" alt="envol">
+    </div>
+    <div class="text-episode">
+      <h3><?= $episodes[0]->getTitre()?></h3>
+      <p><?= $episodes[0]->getEpisode()?></p>
+    </div>
+</div>
+
+<div class="all-episode">
+  <div class="image-episode">
+    <img src="<?= HOST?>public/photos/undraw_true_friends_c94g.svg" alt="amis">
+  </div>
+  <div class="text-episode">
+    <h3><?= $episodes[1]->getTitre()?></h3>
+    <p><?= $episodes[1]->getEpisode()?></p>
+  </div>
+</div>
+
+<div class="all-episode">
+  <div class="image-episode">
+    <img src="<?= HOST?>public/photos/undraw_moonlight_5ksn.svg" alt="lune">
+  </div>
+  <div class="text-episode">
+    <h3><?= $episodes[2]->getTitre()?></h3>
+    <p><?= $episodes[2]->getEpisode()?></p>
+  </div>
+</div>
+
+<div class="all-episode">
+  <div class="image-episode">
+    <img src="<?= HOST?>public/photos/undraw_playing_cards_cywn.svg" alt="carte">
+  </div>
+  <div class="text-episode">
+    <h3><?= $episodes[3]->getTitre()?></h3>
+    <p><?= $episodes[3]->getEpisode()?></p>
+  </div>
+</div>
+
+<div class="all-episode">
+  <div class="image-episode">
+    <img src="<?= HOST?>public/photos/undraw_off_road_9oae.svg" alt="route">
+  </div>
+  <div class="text-episode">
+    <h3><?= $episodes[4]->getTitre()?></h3>
+    <p><?= $episodes[4]->getEpisode()?></p>
+  </div>
+</div>
